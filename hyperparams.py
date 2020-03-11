1119a9ad2dc0bc35b3b408da522f5fd4cc882ccf
kidconan <hyskid801@126.com>
2020-03-11 16:48:11 +0800

    Modify the acoustic property and hypre

--- hyperparams.py
+++ hyperparams.py
@@ -6,10 +6,19 @@ sr = 22050
 # frame_length_ms = 50.
 # frame_shift_ms = 12.5
 preemphasis = 0.97
-frame_shift = 0.0125 # seconds
-frame_length = 0.05 # seconds
-hop_length = int(sr*frame_shift) # samples.
-win_length = int(sr*frame_length) # samples.
+# frame_shift = 0.0125 # seconds
+# frame_length = 0.05 # seconds
+# hop_length = int(sr*frame_shift) # samples.
+# win_length = int(sr*frame_length) # samples.
+
+
+hop_length = 256
+win_length = 1024
+
+frame_shift = hop_length/sr
+frame_length = win_length/sr
+
+
 n_mels = 80 # Number of Mel banks to generate
 power = 1.2 # Exponent for amplifying the predicted magnitude
 min_level_db = -100
@@ -23,11 +32,11 @@ n_iter = 60
 # power = 1.5
 outputs_per_step = 1
 
-epochs = 10000
+epochs = 100000
 lr = 0.001
-save_step = 2000
+save_step = 5000
 image_step = 500
-batch_size = 32
+batch_size = 64
 
 cleaners='english_cleaners'
 
